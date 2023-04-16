ce311ea53db18a159e44eb4ec466cf09741e532f
Philippe Steinmann <philippe.steinmann@gmail.com>
2023-04-16 16:19:12 +0200

    ElfLayoutViewer segmentTypeShortName(): handle missing Interpreter type and cleanup

--- apps/ElfLayoutViewer/src/SegmentTypeString.cpp
+++ apps/ElfLayoutViewer/src/SegmentTypeString.cpp
@@ -10,22 +10,6 @@
 #include "SegmentTypeString.h"
 #include <QLatin1String>
 
-//     Null = 0,                   /*!< PT_NULL: unused program header table entry */
-//     Load = 0x01,                /*!< PT_LOAD: loadable segment */
-//     Dynamic = 0x02,             /*!< PT_DYNAMIC: dynamic linking information */
-//     Interpreter = 0x03,         /*!< PT_INTERP: interpreter */
-//     Note = 0x04,                /*!< PT_NOTE: auxiliary information */
-//     ProgramHeaderTable = 0x06,  /*!< PT_PHDR: program header table */
-//     Tls = 0x07,                 /*!< PT_TLS: thread-Local Storage template */
-//     Unknown = 0x10000000,       /*!< Not from the standard */
-//     GnuEhFrame = 0x6474e550,    /*!< PT_GNU_EH_FRAME: the array element specifies the location and size
-//                                       of the exception handling information as defined by the .eh_frame_hdr section. */
-//     GnuStack = 0x6474e551,      /*!< PT_GNU_STACK: the p_flags member specifies the permissions on the segment
-//                                      containing the stack and is used to indicate wether the stack should be executable.
-//                                      The absense of this header indicates that the stack will be executable. */
-//     GnuRelRo = 0x6474e552        /*!< PT_GNU_RELRO: the array element specifies the location and size
-//                                      of a segment which may be made read-only after relocation shave been processed. */
-
 
 QString segmentTypeShortName(Mdt::ExecutableFile::Elf::SegmentType type) noexcept
 {
@@ -38,6 +22,8 @@ QString segmentTypeShortName(Mdt::ExecutableFile::Elf::SegmentType type) noexcep
       return QLatin1String("LOAD");
     case SegmentType::Dynamic:
       return QLatin1String("DYNAMIC");
+    case SegmentType::Interpreter:
+      return QLatin1String("INTERP");
     case SegmentType::Note:
       return QLatin1String("NOTE");
     case SegmentType::ProgramHeaderTable:
